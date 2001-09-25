a32f5e3b3064c4298fe7678b97c45771e6cac2b4
knik <knik>
2001-09-25 17:39:36 +0000

    removed unneeded declarations

--- src/win32/main.h
+++ src/win32/main.h
@@ -1,12 +1,10 @@
 /* (C) 2000  Krzysztof Nikiel */
-/* $Id: main.h,v 1.1 2001/03/18 07:56:48 knik Exp $ */
+/* $Id: main.h,v 1.2 2001/09/25 17:39:36 knik Exp $ */
 #ifndef A800_WIN_MAIN_H
 #define A800_WIN_MAIN_H
 
 extern char *myname;
 extern HWND hWndMain;
 extern HINSTANCE myInstance;
-extern int bActive;
-extern int vloopexit;
 
 #endif

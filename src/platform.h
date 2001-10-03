d6cdb3359b039252715bef13854c54be4b285e2a
fox <fox>
2001-10-03 16:52:38 +0000

    removed Atari_POT and Atari_Set_LED

--- src/platform.h
+++ src/platform.h
@@ -1,4 +1,4 @@
-/* $Id: platform.h,v 1.4 2001/09/26 10:48:13 fox Exp $ */
+/* $Id: platform.h,v 1.5 2001/10/03 16:52:38 fox Exp $ */
 #ifndef __PLATFORM__
 #define __PLATFORM__
 
@@ -15,15 +15,14 @@ void Atari_DisplayScreen (UBYTE *screen);
 
 int Atari_PORT(int num);
 int Atari_TRIG(int num);
-int Atari_POT(int num);
-#if defined(SET_LED) && defined(NO_LED_ON_SCREEN)
-void Atari_Set_LED(int how);
-#endif
 
 #endif
 
 /*
 $Log: platform.h,v $
+Revision 1.5  2001/10/03 16:52:38  fox
+removed Atari_POT and Atari_Set_LED
+
 Revision 1.4  2001/09/26 10:48:13  fox
 removed Atari_PEN
 

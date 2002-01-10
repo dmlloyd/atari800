6a81578ab39615387990868f923cdf5646911143
joy <joy>
2002-01-10 11:50:10 +0000

    include zlib.h from the system include path and not from local directory

--- src/statesav.c
+++ src/statesav.c
@@ -1,4 +1,4 @@
-/* $Id: statesav.c,v 1.4 2001/09/17 18:13:35 fox Exp $ */
+/* $Id: statesav.c,v 1.5 2002/01/10 11:50:10 joy Exp $ */
 /* State saving module */
 
 /* Include files */
@@ -11,7 +11,7 @@
 #include "config.h"
 #include "log.h"
 #ifdef HAVE_LIBZ
-#include "zlib.h"
+#include <zlib.h>
 #endif
 
 #define SAVE_VERSION_NUMBER	3
@@ -432,6 +432,9 @@ int ReadAtariState( char *filename, const char *mode )
 
 /*
 $Log: statesav.c,v $
+Revision 1.5  2002/01/10 11:50:10  joy
+include zlib.h from the system include path and not from local directory
+
 Revision 1.4  2001/09/17 18:13:35  fox
 machine, mach_xlxe, Ram256, os, default_system -> machine_type, ram_size
 
